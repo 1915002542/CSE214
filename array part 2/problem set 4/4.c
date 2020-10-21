e3e9d75925d918775637dc7545b568117b159eda
1915002542 <72740507+1915002542@users.noreply.github.com>
2020-10-21 20:40:49 +0600

    Add files via upload

---
+++ array part 2/problem set 4/4.c
@@ -0,0 +1,24 @@
+/*Print entire an array each element in new line .....
+Razia Sultana Himu
+ID-1915002542
+*/
+#include<stdio.h>
+void main(){
+
+int a[100],i;
+
+a[0]=10;
+a[1]=20;
+a[2]=30;
+a[3]=40;
+a[4]=50;
+a[5]=60;
+a[6]=70;
+a[7]=80;
+a[8]=90;
+a[9]=100;
+
+for(i=0;i<10;i++){
+printf("\n%d",a[i]);
+}
+}

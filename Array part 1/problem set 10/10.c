54bb2ea20e6ec7d2e009c51f8d8ea51dadcb7790
1915002542 <72740507+1915002542@users.noreply.github.com>
2020-10-21 20:31:43 +0600

    Add files via upload

---
+++ Array part 1/problem set 10/10.c
@@ -0,0 +1,45 @@
+/*Delete a value from before kth index(take input k from user) from array shift all other to left .....
+Razia Sultana Himu
+ID-1915002542
+*/
+#include <stdio.h>
+#define MAX_SIZE 100
+int main()
+{
+    int arr[MAX_SIZE];
+    int i, size, pos;
+
+    printf("Enter size of the array : ");
+    scanf("%d", &size);
+    printf("Enter elements in array : ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element position to delete : ");
+    scanf("%d", &pos);
+
+
+    if(pos < 0 || pos > size)
+    {
+        printf("Invalid position! Please enter position between 1 to %d", size);
+    }
+    else
+    {
+
+        for(i=pos-1; i<size-1; i++)
+        {
+            arr[i] = arr[i + 1];
+        }
+
+        size--;
+
+        printf("\nElements of array after delete are : ");
+        for(i=0; i<size; i++)
+        {
+            printf("%d\t", arr[i]);
+        }
+    }
+    return 0;
+}
